7e77321c23217abb4abaec84c10658c70827de7f
Rukawalee <Rukawalee@vip.qq.com>
2020-11-24 22:31:44 +0800

    update string util for substring

--- src/main/java/com/rukawa/common/util/StringUtil.java
+++ src/main/java/com/rukawa/common/util/StringUtil.java
@@ -16,4 +16,19 @@ public class StringUtil {
     public static boolean isEmpty(String str) {
         return !Optional.ofNullable(str).isPresent();
     }
+
+    /**
+     * 截取包含最后一个关键字之前的字符串，不包含关键字所在位置
+     *
+     * @param src 源字符串
+     * @param key 关键字
+     * @return 子串
+     */
+    public static String substringByLastKey(String src, String key) {
+        int offset = src.lastIndexOf(key);
+        if (offset != -1) {
+            return src.substring(0, offset);
+        }
+        return src;
+    }
 }
