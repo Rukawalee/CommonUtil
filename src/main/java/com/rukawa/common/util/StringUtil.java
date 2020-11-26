c1dd31213a29046ae0f0efd02209cbc5f08e6937
Rukawalee <Rukawalee@vip.qq.com>
2020-11-26 22:18:49 +0800

    for String util to add unique code

--- src/main/java/com/rukawa/common/util/StringUtil.java
+++ src/main/java/com/rukawa/common/util/StringUtil.java
@@ -1,6 +1,8 @@
 package com.rukawa.common.util;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.UUID;
 
 /**
  * 字符串校验
@@ -11,10 +13,31 @@ public class StringUtil {
      * 判断空字符串
      *
      * @param str 传入字符串
-     * @return 返回bool
+     * @return bool
      */
     public static boolean isEmpty(String str) {
         return !Optional.ofNullable(str).isPresent();
+    }
+
+    /**
+     * 生成28位长度的唯一码
+     *
+     * @return String
+     */
+    public static String generateUniqueCode() {
+        UUID uuid = UUID.randomUUID();
+        LocalDateTime now = LocalDateTime.now();
+        // 获取UUID前14位
+        String preUUID = uuid.toString()
+                .replaceAll("-", "")
+                .substring(0, 14);
+        // 获取到秒的14位数
+        String time = now.toString()
+                .replaceAll("[-T:.]", "")
+                .substring(0, 14);
+        StringBuilder uniqueCodeBuilder = new StringBuilder(preUUID);
+        uniqueCodeBuilder.append(time);
+        return uniqueCodeBuilder.toString();
     }
 
     /**
